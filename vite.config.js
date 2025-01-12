79b8f9e3a3fc48f8b00c85ee344fad2db7ff04bb
Ruby He <dreamahourse@gmail.com>
2025-01-12 19:51:55 +0800

    修改viteconfig.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/navigate-station/",
-  build: {
-    outDir: "dist",
-  },
 });
