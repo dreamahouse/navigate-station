1677077e0077771448be4c127f5d5c8920380f93
Ruby He <dreamahourse@gmail.com>
2025-01-12 20:05:22 +0800

    修改为hashRouter

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import HomePage from "./pages/HomePage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: "/",
         element: <HomePage />,
